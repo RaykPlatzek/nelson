fae37a4b7992c21390982aafe1b0feebdf136da9
Rayk <rayk@tausolutions.com.au>
2018-09-20 13:19:15 +0200

    Added seed data, setting migration defaults to 0

--- db/migrate/20180514095001_create_pairs.rb
+++ db/migrate/20180514095001_create_pairs.rb
@@ -2,10 +2,10 @@ class CreatePairs < ActiveRecord::Migration[5.2]
   def change
     create_table :pairs do |t|
       t.string :code
-      t.decimal :price
-      t.decimal :bid
-      t.decimal :ask
-      t.string :trend
+      t.decimal :price, default: 0
+      t.decimal :bid, default: 0
+      t.decimal :ask, default: 0
+      t.string :trend, default: TREND_STAYED
       t.integer :timestamp, :limit => 8
       t.timestamps
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+["AUDUSD", "AUDEUR", "EURUSD", "USDEUR"].each { |code| Pair.create(code: code) }
