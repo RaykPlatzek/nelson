a7b518dc892888c1fcf5d4adce1f764d277c188b
Rayk <rayk@tausolutions.com.au>
2018-09-22 15:35:01 +0200

    adding sign-up and login pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[ name image])
+    update_attrs = %i[name image]
+    devise_parameter_sanitizer.permit :user_update, keys: update_attrs
+  end
 end
